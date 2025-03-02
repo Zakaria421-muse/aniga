dfdcb57ace9355216b5a773c2d85a8b85ee34896
Zakaria421-muse <zakariamusse51@gmail.com>
2025-03-02 08:24:53 +0300

    Add files via upload
    wucusbyhy

---
+++ f.arrow.js
@@ -0,0 +1,30 @@
+/*const iskuDhufo = (num1,num2) =>{
+    return num1*num2;
+}
+console.log(iskuDhufo(6,3))*/
+let students=[
+    {
+    name:'zakora',
+    joga:'jigjiga',
+    kashqeya:'kafi',
+    wxabarasho:{
+        heer:'jamacad',
+        iskuulSare:'jigjiga high school',
+    },
+    rajaynaya:['guriDhisto','guursado','shaqoFicanHelo'],
+    dada: function(){
+        console.log('25');
+    },
+
+},
+{
+    name:'ali',
+    joga:'muqdisho',
+} ,
+
+
+
+];
+students.forEach((wow)=>{
+    console.log(wow.name)
+});
